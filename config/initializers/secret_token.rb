3aff34ce510ce6e6b8aa61ddf3d1a3195575d31b
Pepe Dorro <pepedorro123456@hotmail.com>
2013-08-23 09:57:17 -0500

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Miproyecto::Application.config.secret_token = '592e814c4615f78a399b3685649346f608cc6db9e5c1670089525f7545a30dcfb5fc234051bc732600ef297ffebea34efc8810a55dc1331c29c8672cf4e26d8a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Miproyecto::Application.config.session_store :cookie_store, key: '_miproyecto_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Miproyecto::Application.config.session_store :active_record_store
